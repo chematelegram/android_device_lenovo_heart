eff293a991f631012cb69aaafa645d1ce7097a1d
chematelegram <chfondia@gmail.com>
2021-12-09 15:47:59 +0100

    heart: Build with Proton clang

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,6 +21,7 @@ ifeq ($(TARGET_PREBUILT_KERNEL),)
   TARGET_KERNEL_CONFIG := lineage_heart_defconfig
   TARGET_KERNEL_CLANG_COMPILE := true
   TARGET_KERNEL_SOURCE := kernel/lenovo/sm8150
+  TARGET_KERNEL_CLANG_VERSION := proton
 endif
 
 # NFC
