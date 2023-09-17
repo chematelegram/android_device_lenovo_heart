b84b102fcf6cac8deff6f4e758496d4efbb83c74
chematelegram <chfondia@gmail.com>
2023-09-17 22:53:16 +0200

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,6 +3,6 @@ git clone https://github.com/chematelegram/device_lenovo_sm8150-common_H.git -b 
 echo 'Cloning vendor tree'
 git clone https://github.com/chematelegram/vendor_lenovo-H.git -b heart-crdroid12 vendor/lenovo
 echo 'Cloning kernel tree'
-git clone https://github.com/crdroidandroid/android_kernel_lenovo_sm8150.git -b 13.0 kernel/lenovo/sm8150
+git clone https://github.com/Evolution-X-Devices/kernel_lenovo_sm8150.git -b tiramisu kernel/lenovo/sm8150 --recurse-submodules
 echo 'Cloning hardware_xiaomi'
 git clone https://github.com/crdroidandroid/android_hardware_xiaomi.git -b 13.0 hardware/xiaomi
